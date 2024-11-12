f8828aa97865e7ad60a2bc8fa1c39ee32c8bb384
thomas-andromeda <thomasandromeda999@gmail.com>
2024-11-12 12:00:06 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header/>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
