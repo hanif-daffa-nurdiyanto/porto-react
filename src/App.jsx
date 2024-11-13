9086220db13fc720ae66af267277842696d09158
thomas-andromeda <thomasandromeda999@gmail.com>
2024-11-13 10:23:07 +0700

    roadmap section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
         <Collaboration />
         <Services/>
         <Pricing />
+        <Roadmap />
       </div>
       <ButtonGradient />
     </>
