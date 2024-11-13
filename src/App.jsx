011119c87a31ca0ac13eddd96bc3b9850b5042ae
thomas-andromeda <thomasandromeda999@gmail.com>
2024-11-13 10:37:57 +0700

    footer section

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
         <Services/>
         <Pricing />
         <Roadmap />
+        <Footer/>
       </div>
       <ButtonGradient />
     </>
